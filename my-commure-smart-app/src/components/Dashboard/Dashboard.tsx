7e43acf99478187d84ae2d1bfdc93187e3602e34
Philip Peterson <philip@commure.com>
2020-12-04 12:47:45 -0500

    Up to some point in chapter 2

--- my-commure-smart-app/src/components/Dashboard/Dashboard.tsx
+++ my-commure-smart-app/src/components/Dashboard/Dashboard.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 import { AppHeader } from "@commure/components-core";
+import { LeftPanelLayout } from "@commure/components-foundation";
 import { PatientList } from "../PatientList/PatientList";
+import PanelBody from "../PanelBody/PanelBody";
 
 const Dashboard: React.FC = (): React.ReactElement => {
   return (
     <>
       <AppHeader appName="My First Commure App" fixedToTop />
-      <div className="app-container">
+      <LeftPanelLayout collapsible panelBody={<PanelBody />}>
+        {/* Spacer */}
+        <div style={{ height: 40 + 16 }}></div>
+
         <PatientList />
-      </div>
+      </LeftPanelLayout>
     </>
   );
 };
